688f5543b3c8fa2df9ffad63e41851e142807d9a
LucasDuarte026 <lucas.sales@usp.br>
2023-08-18 10:00:05 -0300

    inserção das branchs

---
+++ POO/projetos/java/hierarchy_exer/src/Coeficiente.java
@@ -0,0 +1,12 @@
+public class Coeficiente extends Operacao {
+
+    public Coeficiente(float operador_1, float operador_2) {
+        super(operador_1, operador_2);
+    }
+    
+    public float operar(){
+        float result = this.operador_1/this.operador_2;
+        return result;
+    }
+    
+}
