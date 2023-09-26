78e57410d9b83ee9867836f2e0b73bf2db1360a1
LucasDuarte026 <lucas.sales@usp.br>
2023-09-25 23:05:02 -0300

    criação e correção do processo de leitura do csv

--- ED3/1_projeto/main.c
+++ ED3/1_projeto/main.c
@@ -10,13 +10,13 @@ Alunos do curso de graduação de engenharia de computação
 #include <ctype.h>
 #include <stdlib.h>
 
-
 #define LIXO "$"
 #define TAM_REGISTRO_FIXO 21
 #define TAM_REGISTRO 76
+#define MAX_STRING_LENGTH 100
 
-typedef struct main
-{// 13 bytes em status e 4 bytes no resto
+typedef struct
+{ // 13 bytes em status e 4 bytes no resto
     char status;
     int proxRRN;
     int nroTecnologia;
@@ -25,102 +25,120 @@ typedef struct main
 
 typedef struct
 {
-
     int tamanho;
     char *string;
 
 } StringVariavel;
 
-typedef struct {
-    char removido; // 1 byte
-    int grupo; // 4 bytes
-    int popularidade; // 4 bytes
-    int peso; // 4 bytes
-    StringVariavel nomeTecnologiaOrigem; // tamanho variável
+typedef struct
+{
+    char removido;                        // 1 byte
+    int grupo;                            // 4 bytes
+    int popularidade;                     // 4 bytes
+    int peso;                             // 4 bytes
+    StringVariavel nomeTecnologiaOrigem;  // tamanho variável
     StringVariavel nomeTecnologiaDestino; // tamanho variável
 
 } Dados;
 
-void readline(char* string){
+void readline(char *string)
+{
     char c = 0;
 
-    do{
-        c = (char) getchar();
+    do
+    {
+        c = (char)getchar();
 
-    } while(c == '\n' || c == '\r');
+    } while (c == '\n' || c == '\r');
 
     int i = 0;
 
-    do{
+    do
+    {
         string[i] = c;
         i++;
         c = getchar();
-    } while(c != '\n' && c != '\r');
+    } while (c != '\n' && c != '\r');
 
-    string[i]  = '\0';
+    string[i] = '\0';
 }
 
-void binarioNaTela(const char *nomeArquivoBinario) { /* Você não precisa entender o código dessa função. */
+void binarioNaTela(const char *nomeArquivoBinario)
+{ /* Você não precisa entender o código dessa função. */
 
-	/* Use essa função para comparação no run.codes. Lembre-se de ter fechado (fclose) o arquivo anteriormente.
-	*  Ela vai abrir de novo para leitura e depois fechar (você não vai perder pontos por isso se usar ela). */
+    /* Use essa função para comparação no run.codes. Lembre-se de ter fechado (fclose) o arquivo anteriormente.
+     *  Ela vai abrir de novo para leitura e depois fechar (você não vai perder pontos por isso se usar ela). */
 
-	unsigned long i, cs;
-	unsigned char *mb;
-	size_t fl;
-	FILE *fs;
-	if(nomeArquivoBinario == NULL || !(fs = fopen(nomeArquivoBinario, "rb"))) {
-		fprintf(stderr, "ERRO AO ESCREVER O BINARIO NA TELA (função binarioNaTela): não foi possível abrir o arquivo que me passou para leitura. Ele existe e você tá passando o nome certo? Você lembrou de fechar ele com fclose depois de usar?\n");
-		return;
-	}
-	fseek(fs, 0, SEEK_END);
-	fl = ftell(fs);
-	fseek(fs, 0, SEEK_SET);
-	mb = (unsigned char *) malloc(fl);
-	fread(mb, 1, fl, fs);
+    unsigned long i, cs;
+    unsigned char *mb;
+    size_t fl;
+    FILE *fs;
+    if (nomeArquivoBinario == NULL || !(fs = fopen(nomeArquivoBinario, "rb")))
+    {
+        fprintf(stderr, "ERRO AO ESCREVER O BINARIO NA TELA (função binarioNaTela): não foi possível abrir o arquivo que me passou para leitura. Ele existe e você tá passando o nome certo? Você lembrou de fechar ele com fclose depois de usar?\n");
+        return;
+    }
+    fseek(fs, 0, SEEK_END);
+    fl = ftell(fs);
+    fseek(fs, 0, SEEK_SET);
+    mb = (unsigned char *)malloc(fl);
+    fread(mb, 1, fl, fs);
 
-	cs = 0;
-	for(i = 0; i < fl; i++) {
-		cs += (unsigned long) mb[i];
-	}
-	printf("%lf\n", (cs / (double) 100));
-	free(mb);
-	fclose(fs);
+    cs = 0;
+    for (i = 0; i < fl; i++)
+    {
+        cs += (unsigned long)mb[i];
+    }
+    printf("%lf\n", (cs / (double)100));
+    free(mb);
+    fclose(fs);
 }
 
-void scan_quote_string(char *str) {
+void scan_quote_string(char *str)
+{
 
-	/*
-	*	Use essa função para ler um campo string delimitado entre aspas (").
-	*	Chame ela na hora que for ler tal campo. Por exemplo:
-	*
-	*	A entrada está da seguinte forma:
-	*		nomeDoCampo "MARIA DA SILVA"
-	*
-	*	Para ler isso para as strings já alocadas str1 e str2 do seu programa, você faz:
-	*		scanf("%s", str1); // Vai salvar nomeDoCampo em str1
-	*		scan_quote_string(str2); // Vai salvar MARIA DA SILVA em str2 (sem as aspas)
-	*
-	*/
+    /*
+     *	Use essa função para ler um campo string delimitado entre aspas (").
+     *	Chame ela na hora que for ler tal campo. Por exemplo:
+     *
+     *	A entrada está da seguinte forma:
+     *		nomeDoCampo "MARIA DA SILVA"
+     *
+     *	Para ler isso para as strings já alocadas str1 e str2 do seu programa, você faz:
+     *		scanf("%s", str1); // Vai salvar nomeDoCampo em str1
+     *		scan_quote_string(str2); // Vai salvar MARIA DA SILVA em str2 (sem as aspas)
+     *
+     */
 
-	char R;
+    char R;
 
-	while((R = getchar()) != EOF && isspace(R)); // ignorar espaços, \r, \n...
+    while ((R = getchar()) != EOF && isspace(R))
+        ; // ignorar espaços, \r, \n...
 
-	if(R == 'N' || R == 'n') { // campo NULO
-		getchar(); getchar(); getchar(); // ignorar o "ULO" de NULO.
-		strcpy(str, ""); // copia string vazia
-	} else if(R == '\"') {
-		if(scanf("%[^\"]", str) != 1) { // ler até o fechamento das aspas
-			strcpy(str, "");
-		}
-		getchar(); // ignorar aspas fechando
-	} else if(R != EOF){ // vc tá tentando ler uma string que não tá entre aspas! Fazer leitura normal %s então, pois deve ser algum inteiro ou algo assim...
-		str[0] = R;
-		scanf("%s", &str[1]);
-	} else { // EOF
-		strcpy(str, "");
-	}
+    if (R == 'N' || R == 'n')
+    { // campo NULO
+        getchar();
+        getchar();
+        getchar();       // ignorar o "ULO" de NULO.
+        strcpy(str, ""); // copia string vazia
+    }
+    else if (R == '\"')
+    {
+        if (scanf("%[^\"]", str) != 1)
+        { // ler até o fechamento das aspas
+            strcpy(str, "");
+        }
+        getchar(); // ignorar aspas fechando
+    }
+    else if (R != EOF)
+    { // vc tá tentando ler uma string que não tá entre aspas! Fazer leitura normal %s então, pois deve ser algum inteiro ou algo assim...
+        str[0] = R;
+        scanf("%s", &str[1]);
+    }
+    else
+    { // EOF
+        strcpy(str, "");
+    }
 }
 
 // !!
@@ -131,35 +149,28 @@ FILE *init_bin(FILE *bin)
     cabecalho.proxRRN = 0;
     cabecalho.nroParesTecnologia = 0;
     cabecalho.nroTecnologia = 0;
-    
+
     fwrite(&cabecalho.status, sizeof(char), 1, bin);
 
     return bin;
 }
-FILE *archive_open(const char *nameArchive)
-{
-    // entrada do arquivos seu retorno em ponteiro
-    FILE *file;
-    file = fopen(nameArchive, "r");
 
-    return file;
-}
-
-void Atualizar_Cabecalho(FILE *bin, int nroTecnologia, int nroParesTecnologia)
+void Atualizar_Cabecalho(FILE *bin, int nroTecnologia, int nroParesTecnologia, int prox)
 {
-    fseek(bin, 0, SEEK_SET);
     Cabecalho cabecalho;
+    fseek(bin, 0, SEEK_SET); // volta para o inicio
     fread(&cabecalho, sizeof(Cabecalho), 1, bin);
 
-    cabecalho.status = '1'; 
+    cabecalho.status = '1';
+    cabecalho.proxRRN = prox;
     cabecalho.nroTecnologia = nroTecnologia;
     cabecalho.nroParesTecnologia = nroParesTecnologia;
-    
-    fseek(bin, 0, SEEK_SET); //volta para o inicio
+
+    fseek(bin, 0, SEEK_SET); // volta para o inicio
     fwrite(&cabecalho, sizeof(Cabecalho), 1, bin);
 }
 
-//funcao nova
+// funcao nova
 void Escrever_Dados(FILE *bin, Dados dados)
 {
     fwrite(&dados.removido, sizeof(char), 1, bin);
@@ -172,49 +183,42 @@ void Escrever_Dados(FILE *bin, Dados dados)
     fwrite(&dados.nomeTecnologiaDestino.string, sizeof(char), dados.nomeTecnologiaDestino.tamanho, bin);
 }
 
-void printarRNN(FILE *bin, int RNN)
-{
-
-    unsigned char byte;
-    while (fread(&byte, sizeof(byte), 1, bin) == 1)
-    {
-        printf("Byte lido: %c\n", byte);
-    }
-}
-
-void printar_binario(FILE *bin)
-{
-    int RNN = 3;
-
-    printarRNN(bin, RNN);
-}
-
-
-Dados LerRegistroCSV(FILE *csv){ // le o registro
+Dados LerRegistroCSV(FILE *csv)
+{ // le o registro
     Dados dados;
-    char buffer[100];
-
     dados.removido = '-';
     dados.nomeTecnologiaOrigem.string = NULL;
+    dados.nomeTecnologiaOrigem.tamanho = 0;
     dados.nomeTecnologiaDestino.string = NULL;
+    dados.nomeTecnologiaDestino.tamanho = 0;
 
-    fscanf(csv, "%d,%d,%d,", &dados.grupo, &dados.popularidade, &dados.peso);
-    scan_quote_string(buffer);
-    dados.nomeTecnologiaOrigem.tamanho = strlen(buffer);
-    dados.nomeTecnologiaOrigem.string = strdup(buffer);
-    // scan_quote_string(buffer);
-    // dados.nomeTecnologiaDestino.tamanho = strlen(buffer);
-    // dados.nomeTecnologiaDestino.string = strdup(buffer);
+    char *nomeTecnologiaOrigem;
+    int grupo;
+    int popularidade;
+    char *nomeTecnologiaDestino;
+    int peso;
+
+    char bufferOrigem[MAX_STRING_LENGTH];
+    char bufferDestino[MAX_STRING_LENGTH];
+
+    if (fscanf(csv, " %[^,],%d,%d, %[^,],%d", bufferOrigem, &dados.grupo, &dados.popularidade, bufferDestino, &dados.peso) == 5)
+    {
+        dados.nomeTecnologiaOrigem.tamanho = strlen(bufferOrigem); // documentar pq funciona
+        dados.nomeTecnologiaOrigem.string = strdup(bufferOrigem);
+
+        dados.nomeTecnologiaDestino.tamanho = strlen(bufferDestino);
+        dados.nomeTecnologiaDestino.string = strdup(bufferDestino);
+            printf("%s,%d,%d,%s,%d\n",  dados.nomeTecnologiaOrigem.string, dados.grupo, dados.popularidade, dados.nomeTecnologiaDestino.string, dados.peso);
+    }
 
     return dados;
 }
 
-
 short int functionality_1(const char csvArchiveName[], const char binArchiveName[])
-{   
-    printf("\n\n\tcsv:%s\t\tbinary:\t%s\n",csvArchiveName,binArchiveName);
-    FILE *csv = archive_open(csvArchiveName); // buscar o csv
-    FILE *bin = fopen(binArchiveName, "wb+");  // criar o bin
+{
+    printf("\n\n\tcsv:%s\t\tbinary:\t%s\n", csvArchiveName, binArchiveName);
+    FILE *csv = fopen(csvArchiveName, "r");  // buscar o csv
+    FILE *bin = fopen(binArchiveName, "wb"); // criar o bin
 
     if (csv == NULL || bin == NULL)
     {
@@ -225,16 +229,20 @@ short int functionality_1(const char csvArchiveName[], const char binArchiveName
     bin = init_bin(bin); // inicializar o bin
 
     Dados dados;
-    while(!feof(csv)){
+
+    char headerLine[MAX_STRING_LENGTH];
+    fgets(headerLine, sizeof(headerLine), csv); // pular a primeira linha
+    while (!feof(csv))
+    {
         dados = LerRegistroCSV(csv);
         Escrever_Dados(bin, dados);
 
         free(dados.nomeTecnologiaOrigem.string);
         free(dados.nomeTecnologiaDestino.string);
 
-        Atualizar_Cabecalho(bin, 1, 1); //Aqui ajustar ainda !!!!!!---
-
-        
+        // int proxRNN = 10;
+        // Atualizar_Cabecalho(bin, 1, 1, proxRNN); // Aqui ajustar ainda !!!!!!---
+        // init = 0;
     }
     fclose(csv);
     fclose(bin);
@@ -266,9 +274,8 @@ int main(int argc, char const *argv[])
 
     printf("os argumentos são \t%s\t%s\t%s\n", argumento_1, argumento_2, argumento_3);
 
-    
-    const char csvArchiveName[] = "tecnologia_curtoTESTE.csv";   // inserção manual do nome dos arquivos para debug
-    const char binArchiveName[] = "output_file.bin";  // inserção manual do nome dos arquivos para debug
+    const char csvArchiveName[] = "tecnologia.csv";  // inserção manual do nome dos arquivos para debug
+    const char binArchiveName[] = "output_file.bin"; // inserção manual do nome dos arquivos para debug
 
     // const char *csvArchiveName = argumento_2;
     // const char *binArchiveName = argumento_3;
@@ -299,7 +306,7 @@ int main(int argc, char const *argv[])
     // fopen
 
     //  -- - -- -- - -- -- - -- -- - -- -- - -- -- - -- -- - -- -- - -- -- - --
-    printf("\n\t\tSucesso\n"); // apagar ao fim
+    printf("\n\t\tSucesso\n");                                     // apagar ao fim
     printf("\n\n-------- -------- -------- -------- -------- \n"); // apagar ao fim
     return 0;
 }
