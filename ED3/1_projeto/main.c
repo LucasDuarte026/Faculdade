6d935b643f7a148a6d7c9ba216d92d7bff59f44d
LucasDuarte026 <lucas.sales@usp.br>
2023-10-03 10:15:37 -0300

    Atualizado até a func 3 tudo na main

--- ED3/1_projeto/main.c
+++ ED3/1_projeto/main.c
@@ -149,7 +149,7 @@ FILE *init_bin(FILE *bin)
     cabecalho.nroParesTecnologia = 0;
     cabecalho.nroTecnologia = 0;
 
-    fwrite(&cabecalho.status, sizeof(char), 1, bin);
+    fwrite(&cabecalho, sizeof(char), 1, bin);
 
     return bin;
 }
@@ -175,10 +175,23 @@ void Escrever_Dados(FILE *bin, Dados dados)
     fwrite(&dados.grupo, sizeof(int), 1, bin);
     fwrite(&dados.popularidade, sizeof(int), 1, bin);
     fwrite(&dados.peso, sizeof(int), 1, bin);
+
     fwrite(&dados.nomeTecnologiaOrigem.tamanho, sizeof(int), 1, bin);
-    fwrite(&dados.nomeTecnologiaOrigem.string, sizeof(char), dados.nomeTecnologiaOrigem.tamanho, bin);
+    fwrite(dados.nomeTecnologiaOrigem.string, sizeof(char), dados.nomeTecnologiaOrigem.tamanho, bin);
+
     fwrite(&dados.nomeTecnologiaDestino.tamanho, sizeof(int), 1, bin);
-    fwrite(&dados.nomeTecnologiaDestino.string, sizeof(char), dados.nomeTecnologiaDestino.tamanho, bin);
+    fwrite(dados.nomeTecnologiaDestino.string, sizeof(char), dados.nomeTecnologiaDestino.tamanho, bin);
+
+    int tamanhoAtual = sizeof(char) + 3 * sizeof(int) + 2 * sizeof(int) + dados.nomeTecnologiaOrigem.tamanho + dados.nomeTecnologiaDestino.tamanho;
+
+    // Calcule a quantidade de lixo
+    int quantidadeLixo = TAM_REGISTRO - tamanhoAtual;
+
+    // Escreva o lixo no arquivo binário
+    for (int i = 0; i < quantidadeLixo; i++)
+    {
+        fwrite(LIXO, sizeof(char), 1, bin);
+    }
 }
 
 Dados LerRegistroCSV(FILE *csv)
@@ -212,19 +225,23 @@ Dados LerRegistroCSV(FILE *csv)
     return dados;
 }
 
-char **testa_unico(int *prt_quant_tec, Dados dado, char **tecnologies) {
+char **testa_unico(int *prt_quant_tec, Dados dado, char **tecnologies)
+{
     int _quant = *prt_quant_tec;
     int flag = 0;
 
     // Verifica se dado.nomeTecnologiaOrigem.string já existe em tecnologies
-    for (int i = 0; i < *prt_quant_tec; i++) {
-        if (strcmp(dado.nomeTecnologiaOrigem.string, tecnologies[i]) == 0 || strcmp(dado.nomeTecnologiaDestino.string, tecnologies[i]) == 0 ) {
+    for (int i = 0; i < *prt_quant_tec; i++)
+    {
+        if (strcmp(dado.nomeTecnologiaOrigem.string, tecnologies[i]) == 0 || strcmp(dado.nomeTecnologiaDestino.string, tecnologies[i]) == 0)
+        {
             flag = 1; // Tecnologia já existe
             break;
         }
     }
 
-    if (!flag) {
+    if (!flag)
+    {
         _quant++;
         tecnologies = realloc(tecnologies, _quant * sizeof(char *));
         tecnologies[_quant - 1] = strdup(dado.nomeTecnologiaOrigem.string);
@@ -288,7 +305,7 @@ short int Functionality_1(const char csvArchiveName[], const char binArchiveName
     {
         Dados dados;
         dados = LerRegistroCSV(csv); //  Lê o arquivo linha a linha e armazana em dados
-        Escrever_Dados(bin, dados);  //  Escreve o dado no arquivo binário
+        // Escrever_Dados(bin, dados);  //  Escreve o dado no arquivo binário
         tecnologies = testa_unico(&quant_tec, dados, tecnologies);
         pares = testa_par(&duplicade_quant_tec, dados, pares);
 
@@ -314,9 +331,9 @@ short int Functionality_1(const char csvArchiveName[], const char binArchiveName
     return 0;
 }
 
-void Recuperar_Dados(const char binArchiveName[])
+void functionality_2(const char binArchiveName[])
 {
-    FILE *bin = fopen(binArchiveName, "rb"); // abrir o bin para leitura
+    FILE *bin = fopen(binArchiveName, "rb+"); // abrir o bin para leitura
     if (bin == NULL)
     {
         printf("\nFalha no processamento do arquivo.\n");
@@ -336,10 +353,12 @@ void Recuperar_Dados(const char binArchiveName[])
     }
 
     Dados dados;
+    int registros_lidos = 0;
     while (fread(&dados.removido, sizeof(char), 1, bin))
     {
         if (dados.removido == '-')
         { // Se o registro não estiver removido logicamente
+
             fread(&dados.grupo, sizeof(int), 1, bin);
             fread(&dados.popularidade, sizeof(int), 1, bin);
             fread(&dados.peso, sizeof(int), 1, bin);
@@ -355,12 +374,13 @@ void Recuperar_Dados(const char binArchiveName[])
             dados.nomeTecnologiaDestino.string[dados.nomeTecnologiaDestino.tamanho] = '\0';
 
             // Exibir os dados
-            // printf("Grupo: %d\n", dados.grupo);
-            // printf("Popularidade: %d\n", dados.popularidade);
-            // printf("Peso: %d\n", dados.peso);
-            // printf("Nome Tecnologia Origem: %s\n", dados.nomeTecnologiaOrigem.string);
-            // printf("Nome Tecnologia Destino: %s\n", dados.nomeTecnologiaDestino.string);
-            // printf("-------------------------------\n");
+            printf("Grupo: %d\n", dados.grupo);
+            printf("Popularidade: %d\n", dados.popularidade);
+            printf("Peso: %d\n", dados.peso);
+            printf("Nome Tecnologia Origem: %s\n", dados.nomeTecnologiaOrigem.string);
+            printf("Nome Tecnologia Destino: %s\n", dados.nomeTecnologiaDestino.string);
+            printf("-------------------------------\n");
+            registros_lidos++;
 
             // Liberar memória alocada para strings
             free(dados.nomeTecnologiaOrigem.string);
@@ -370,6 +390,88 @@ void Recuperar_Dados(const char binArchiveName[])
         {
             // Se o registro estiver removido logicamente, pular para o próximo registro
             fseek(bin, TAM_REGISTRO - sizeof(char), SEEK_CUR);
+        }
+    }
+
+    if (registros_lidos == 0)
+    {
+        printf("Nenhum registro válido encontrado no arquivo.\n");
+    }
+
+    fclose(bin);
+}
+
+void functionality_3(const char binArchiveName[], int n)
+{
+    FILE *bin = fopen(binArchiveName, "rb");
+    if (bin == NULL)
+    {
+        printf("\nFalha no processamento do arquivo.\n");
+        return;
+    }
+
+    Cabecalho cabecalho;
+    fread(&cabecalho, sizeof(Cabecalho), 1, bin);
+
+    if (cabecalho.status == '0')
+    {
+        printf("\nArquivo binário não está consistente.\n");
+        fclose(bin);
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        char nomeCampo[MAX_STRING_LENGTH];
+        char valorCampo[MAX_STRING_LENGTH];
+        scanf("%s", nomeCampo);
+        scan_quote_string(valorCampo);
+
+        int found = 0;
+        Dados dados;
+
+        fseek(bin, sizeof(Cabecalho), SEEK_SET);
+
+        while (fread(&dados.removido, sizeof(char), 1, bin))
+        {
+            if (dados.removido == '-')
+            {
+                fread(&dados.grupo, sizeof(int), 1, bin);
+                fread(&dados.popularidade, sizeof(int), 1, bin);
+                fread(&dados.peso, sizeof(int), 1, bin);
+
+                fread(&dados.nomeTecnologiaOrigem.tamanho, sizeof(int), 1, bin);
+                dados.nomeTecnologiaOrigem.string = (char *)malloc(dados.nomeTecnologiaOrigem.tamanho + 1);
+                fread(dados.nomeTecnologiaOrigem.string, sizeof(char), dados.nomeTecnologiaOrigem.tamanho, bin);
+                dados.nomeTecnologiaOrigem.string[dados.nomeTecnologiaOrigem.tamanho] = '\0';
+
+                fread(&dados.nomeTecnologiaDestino.tamanho, sizeof(int), 1, bin);
+                dados.nomeTecnologiaDestino.string = (char *)malloc(dados.nomeTecnologiaDestino.tamanho + 1);
+                fread(dados.nomeTecnologiaDestino.string, sizeof(char), dados.nomeTecnologiaDestino.tamanho, bin);
+                dados.nomeTecnologiaDestino.string[dados.nomeTecnologiaDestino.tamanho] = '\0';
+
+                if ((strcmp(nomeCampo, "nomeTecnologiaOrigem") == 0 && strcmp(valorCampo, dados.nomeTecnologiaOrigem.string) == 0) ||
+                    (strcmp(nomeCampo, "nomeTecnologiaDestino") == 0 && strcmp(valorCampo, dados.nomeTecnologiaDestino.string) == 0) ||
+                    (strcmp(nomeCampo, "grupo") == 0 && atoi(valorCampo) == dados.grupo) ||
+                    (strcmp(nomeCampo, "popularidade") == 0 && atoi(valorCampo) == dados.popularidade) ||
+                    (strcmp(nomeCampo, "peso") == 0 && atoi(valorCampo) == dados.peso))
+                {
+                    printf("%s,%d,%d,%s,%d\n", dados.nomeTecnologiaOrigem.string, dados.grupo, dados.popularidade, dados.nomeTecnologiaDestino.string, dados.peso);
+                    found = 1;
+                }
+
+                free(dados.nomeTecnologiaOrigem.string);
+                free(dados.nomeTecnologiaDestino.string);
+            }
+            else
+            {
+                fseek(bin, TAM_REGISTRO - sizeof(char), SEEK_CUR);
+            }
+        }
+
+        if (!found)
+        {
+            printf("Registro inexistente.\n");
         }
     }
 
@@ -395,28 +497,38 @@ int main(int argc, char const *argv[])
     const char *argumento_1 = argv[1];
     const char *argumento_2 = argv[2];
     const char *argumento_3 = argv[3];
+    // const char *argumento_4 = argv[4];
+    // const char *argumento_5 = argv[5];
 
     // printf("os argumentos são \t%s\t%s\t%s\n", argumento_1, argumento_2, argumento_3);  //   Ver o que está sendo lido no momento
 
-    const char csvArchiveName[] = "tecnologia.csv";  // inserção manual do nome dos arquivos para debug
-    const char binArchiveName[] = "output_file.bin"; // inserção manual do nome dos arquivos para debug
+    // const char csvArchiveName[] = "tecnologia.csv";  // inserção manual do nome dos arquivos para debug
+    // const char binArchiveName[] = "output_file.bin"; // inserção manual do nome dos arquivos para debug
 
-    // const char *csvArchiveName = argumento_2;
-    // const char *binArchiveName = argumento_3;
+    const char *csvArchiveName;
+    const char *binArchiveName;
+
+    int n;
     switch (decision)
     {
     case '1':
+        csvArchiveName = argumento_2;
+        binArchiveName = argumento_3;
         if (Functionality_1(csvArchiveName, binArchiveName))
         {
             printf("\nFalha no processamento do arquivo.");
         };
         break;
     case '2': // Supondo que a opção 2 seja para recuperar e exibir os dados
-        Recuperar_Dados(binArchiveName);
+        binArchiveName = argumento_2;
+        functionality_2(binArchiveName);
         break;
 
     case '3':
-        /* code */
+        n = argumento_3[0]-48;
+        binArchiveName = argumento_2;
+
+        functionality_3(binArchiveName, n);
         break;
     case '4':
         /* code */
